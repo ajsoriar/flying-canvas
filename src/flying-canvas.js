cc78aef36f89eafb09f572abca6bdc9bfacc18a3
asr <ajsoriar@gmail.com>
2019-04-05 00:18:40 +0200

    css

--- src/flying-canvas.js
+++ src/flying-canvas.js
@@ -1,3 +1,12 @@
+/**
+ * flying-canvas
+ * Flying Canvas.
+ * @version v0.0.1 - 2019-04-04
+ * @link https://github.com/ajsoriar/flying-canvas
+ * @author Andres J. Soria R. <ajsoriar@gmail.com>
+ * @license MIT License, http://www.opensource.org/licenses/MIT
+ */
+
 (function (scope) {
     "use strict";
     var fc = function () {
